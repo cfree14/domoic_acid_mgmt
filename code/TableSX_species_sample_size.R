4fd9b33eecd5b3586f0493f5a3879e6cca269ce6
Chris Free <cfree14@gmail.com>
2021-04-26 12:30:32 -0700

    sample size table and table rearrange

---
+++ code/TableSX_species_sample_size.R
@@ -0,0 +1,51 @@
+
+
+
+# Clear workspace
+rm(list = ls())
+
+# Setup
+################################################################################
+
+# Packages
+library(tidyverse)
+
+# Directories
+datadir <- "data/merged/processed"
+plotdir <- "figures"
+tabledir <- "tables"
+
+# Read DA sampling data
+samples_orig <- readRDS(file.path(datadir, "CA_OR_WA_da_sampling_data.Rds"))
+
+
+
+# Buid data
+################################################################################
+
+# Build data
+stats <- samples_orig %>%
+  # Post-2014
+  filter(date >= "2014-01-01") %>%
+  # Summarize
+  group_by(comm_name, sci_name) %>%
+  summarize(states=paste(sort(unique(state)), collapse=", "),
+            n=n()) %>%
+  ungroup() %>%
+  # Arrange
+  arrange(desc(n)) %>%
+  # Recode state
+  mutate(states=recode(states,
+                       "California"="CA",
+                       "California, Oregon"="CA/OR",
+                       "California, Oregon, Washington"="CA/OR/WA",
+                       "Oregon"="OR",
+                       "Washington"="WA")) %>%
+  # Add label
+  mutate(species_label=paste0(comm_name, " (", sci_name, ")")) %>%
+  select(species_label, states, n)
+
+table(stats$states)
+
+# Export
+write.csv(stats, file=file.path(tabledir, "TableSX_species_sample_size.csv"), row.names=F)
